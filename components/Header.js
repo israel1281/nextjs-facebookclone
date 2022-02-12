85aeb3869986a5209a2b166e813d0820d3389887
Israel David <77063876+israel1281@users.noreply.github.com>
2022-02-12 11:40:52 +0000

    init commit

---
+++ components/Header.js
@@ -0,0 +1,68 @@
+import Image from "next/image";
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from '@heroicons/react/solid';
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/outline';
+import HeaderIcon from "./HeaderIcon";
+
+function Header(){
+  const [session] = useSession();
+  return (
+    <div className="flex items-center sticky top-0 z-50 bg-white p-2 lg:px-5 shadow-md">
+      <div className="flex items-center">
+        <img 
+         src='https://links.papareact.com/5me'
+         width={40}
+         height={40}
+         layout="fixed"
+        />
+      </div>
+      <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+        <SearchIcon className="h-6 text-gray-600" />
+        <input className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+         type="text"
+         placeholder="Search Facebook"
+        />
+      </div>
+
+      <div className="flex justify-content flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
+
+      <div className="flex items-center sm:space-x-2 justify-end">
+        <img 
+         className="rounded-full cursor-pointer"
+         src=""
+         width="40"
+         height="40"
+         layout="fixed"
+        />  
+           
+        <p className="whitespace-nowrap font-semibold pr-3">Israel David</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
+
+    </div>
+  )
+}
+  
+export default Header
