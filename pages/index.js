3933fb974ee287714ae916fc6bfd722184889e5c
Israel David <77063876+israel1281@users.noreply.github.com>
2022-02-13 18:27:18 +0000

    init commit

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header"
 import Sidebar from "../components/Sidebar"
+import Feed from "../components/Feed"
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
 
       <main className="flex">
         <Sidebar />
+        <Feed />
       </main>
     </div>
   )
