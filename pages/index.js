caa09f873a8328cbbd495ee91c234f15871fe022
Israel David <77063876+israel1281@users.noreply.github.com>
2022-02-13 16:26:28 +0000

    init commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header"
+import Login from "../components/login"
+import { getSession } from "next-auth/client"
 
-export default function Home() {
+export default function Home({ session }) {
+    if (!session) return <Login />
   return (
     <div className="h-screen bg-gray-100 overflow-hidden">
       <Head>
@@ -18,4 +21,14 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  
+  return {
+    props: {
+      session
+    }
+  }
 }
