dd6b238a860d99c903e993b73d39b819d8dece87
Israel David <77063876+israel1281@users.noreply.github.com>
2022-02-13 16:11:17 +0000

    init commit

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth"
+import providers from "next-auth/providers"
+
+export default NextAuth({
+  providers: [
+    providers.Facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    })
+  ]
+})
