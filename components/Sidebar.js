b152011f2ac4a7ebd45ef8e81404dd5557720ea6
Israel David <77063876+israel1281@users.noreply.github.com>
2022-02-13 17:42:26 +0000

    init commit

---
+++ components/Sidebar.js
@@ -0,0 +1,29 @@
+import {
+  ChevronDownIcon,
+  ShoppingBagIcon,
+  UserGroupIcon,
+} from "@heroicons/react/outline"
+import {
+  CalendarIcon,
+  ClockIcon,
+  DesktopComputerIcon,
+  UsersIcon,
+} from "@heroicons/react/solid"
+import SidebarRow from "./SidebarRow"
+
+function Sidebar() {
+  return (
+    <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
+      <SidebarRow src="content://com.android.chrome.FileProvider/images/screenshot/1644773798561804242064.png" />
+      <SidebarRow Icon={UserIcon} title="Friends" />
+      <SidebarRow Icon={UserGroupIcon} title="Groups" />
+      <SidebarRow Icon={ShoppingBagIcon}  title="Marketplace" />
+      <SidebarRow Icon={DesktopComputerIcon} title="Watch" />
+      <SidebarRow Icon={CalendarIcon} title="Events" />
+      <SidebarRow Icon={ClockIcon} title="Memories" />
+      <SidebarRow Icon={ChevronDownIcon} title="See More" />
+    </div>
+  )
+}
+
+export default Sidebar
